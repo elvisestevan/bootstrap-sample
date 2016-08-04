8416a6ca9c1c3d6e63822cc0f0d3a8ae71cd85da
Elvis Estevan <elvis.estevan65@gmail.com>
2016-08-04 17:13:14 -0300

    Update

--- app.js
+++ app.js
@@ -10,11 +10,6 @@ var port = process.env.PORT;
 
 app.use(express.static(path.resolve(__dirname, 'public')));
 
-//Seta a rota do index
-app.get('/', function(req, res) {
-  res.render('index.html');
-});
-
 http.listen(port, function(){
   console.log(("Servidor rodando na porta " + port));
 });
